a612f43f818fa406044f40d6ba2326af35ce7f0b
Kanstantsin Baltsevich <kostielgreendevil@gmail.com>
2019-10-11 14:17:52 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+        this.solution = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.solution = Math.round((this.min + this.max)/2);
+        return this.solution;
     }
 
     lower() {
-
+        this.max = this.solution;
     }
 
     greater() {
-
+        this.min = this.solution;
     }
 }
 
